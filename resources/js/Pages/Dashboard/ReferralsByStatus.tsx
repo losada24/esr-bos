de84ab27023cf4a98bfe5e7a9d0a3646191b5f71
Efrain Losada <losada24@gmail.com>
2023-09-26 08:39:30 -0400

    Add Pie graph in dashboard

--- resources/js/Pages/Dashboard/ReferralsByStatus.tsx
+++ resources/js/Pages/Dashboard/ReferralsByStatus.tsx
@@ -1,44 +1,73 @@
-import PerfectScrollbar from 'react-perfect-scrollbar'
 import { type ReferralsByStatusCount } from '@/types'
 import { STATUS } from '@/Utils/constants'
+import ReactApexChart from 'react-apexcharts'
 
-const ReferalStatus = ({ status }: { status: ReferralsByStatusCount }) => {
-  const selectedStatus = STATUS.find((s) => s.id === status.status)
-  if (selectedStatus) {
-    return (
-      <div className="flex items-center">
-          <div className="shrink-0 ltr:mr-2 rtl:ml-2 relative z-10">
-              <div className={`${selectedStatus.color} w-8 h-8 rounded-full flex items-center justify-center text-white`}>
-                <p className="text-white text-xs">{status.count}</p>
-              </div>
-          </div>
-          <div>
-              <h5 className="font-semibold dark:text-white-light">{selectedStatus.label}</h5>
-          </div>
-      </div>
-    )
-  }
-
-  return (
-    <>Status Not Found</>
-  )
+interface ReferralsByStatusGraph {
+  series: number[]
+  labels: string[]
+  colors: string[]
 }
 
 const ReferralsByStatus = ({ referralsByStatus }: { referralsByStatus: ReferralsByStatusCount[] }) => {
+  let total: number = 0
+  const referralsByStatusGraph: ReferralsByStatusGraph = {
+    series: [],
+    labels: [],
+    colors: []
+  }
+
+  referralsByStatus.forEach((status) => {
+    const selectedStatus = STATUS.find((s) => s.id === status.status)
+    if (selectedStatus) {
+      total += status.count
+      referralsByStatusGraph.series.push(status.count)
+      referralsByStatusGraph.labels.push(selectedStatus.label)
+      referralsByStatusGraph.colors.push(selectedStatus.hex)
+    }
+  })
+
+  const pieChart: any = {
+    series: referralsByStatusGraph.series,
+    options: {
+      chart: {
+        height: 300,
+        type: 'pie',
+        zoom: {
+          enabled: false
+        },
+        toolbar: {
+          show: false
+        }
+      },
+      labels: referralsByStatusGraph.labels,
+      colors: referralsByStatusGraph.colors,
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            }
+          }
+        }
+      ],
+      stroke: {
+        show: false
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }
+
   return (
     <div className="panel h-full">
       <div className="flex items-start justify-between dark:text-white-light mb-5 -mx-5 p-5 pt-0 border-b  border-white-light dark:border-[#1b2e4b]">
           <h5 className="font-semibold text-lg ">Referrals By Status</h5>
       </div>
-      <PerfectScrollbar className="perfect-scrollbar relative h-[360px] ltr:pr-3 rtl:pl-3 ltr:-mr-3 rtl:-ml-3">
-          <div className="space-y-7">
-            {referralsByStatus.map((status, index) => {
-              return (
-                <ReferalStatus key={`status_${index}`} status={status} />
-              )
-            })}
-          </div>
-      </PerfectScrollbar>
+
+      <ReactApexChart series={pieChart.series} options={pieChart.options} className="rounded-lg bg-white dark:bg-black overflow-hidden" type="pie" height={300} />
+      <p className='text-lg'><span className='font-semibold'>Total:</span>&nbsp;{total}</p>
     </div>
   )
 }
