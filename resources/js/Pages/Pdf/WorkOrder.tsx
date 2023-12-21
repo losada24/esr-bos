6006ce99ee96f09e25427e958640101e386b0726
Efrain Losada <losada24@gmail.com>
2023-12-20 23:28:20 -0500

    Add pdf

--- resources/js/Pages/Pdf/WorkOrder.tsx
+++ resources/js/Pages/Pdf/WorkOrder.tsx
@@ -13,6 +13,10 @@ type IndexOrderProps = PageProps & {
 const tw = createTw({
   theme: {
     extend: {
+      fontFamily: {
+        regular: 'NunitoRegular',
+        bold: 'NunitoBold'
+      },
       colors: {
         custom: '#bada55'
       }
@@ -29,12 +33,12 @@ const WorkOrder = ({ data, auth }: IndexOrderProps) => {
       >
         <Head title="Work Order" />
         <PrintLayout>
-          <Page size="A4" style={tw('p-12')}>
+          <Page size="A4" style={tw('p-12 font-regular')}>
             <View style={tw('p-20 bg-gray-100')}>
               <Text style={tw('text-custom text-3xl')}>Section #1</Text>
             </View>
             <View style={tw('mt-12 px-8 rotate-2')}>
-              <Text style={tw('text-amber-600 text-2xl')}>Section #2</Text>
+              <Text style={tw('text-amber-600 text-2xl font-bold')}>Section #2</Text>
             </View>
           </Page>
         </PrintLayout>
