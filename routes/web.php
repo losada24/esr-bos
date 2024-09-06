278b16edd3b17651d4a250c49da8fd2aa80276e7
Katiuska Cuba <katiuska28@gmail.com>
2024-09-06 14:15:31 -0400

    permits

--- routes/web.php
+++ routes/web.php
@@ -67,7 +67,7 @@ Route::middleware('auth')->group(function () {
       ->middleware(["role:" . RoleEnum::ADMIN->value . '|'. RoleEnum::ACCOUNT_MANAGER->value ]);
 
     Route::get('order/get_delivery_and_installation_date/{payment_factory_date}/{type_of_housing}/{county_id}/{service}', [OrderController::class, 'getDeliveryAndInstallationDate'])
-      ->middleware(["role:" . RoleEnum::ADMIN->value]);
+      ->middleware(["role:" . RoleEnum::ADMIN->value] . '|'. RoleEnum::ACCOUNT_MANAGER->value);
     
     Route::get('dashboard/get_events/{year}/{month}', [DashboardController::class, 'getEvents'])
       ->middleware(["role:" . RoleEnum::ADMIN->value . '|'. RoleEnum::ACCOUNT_MANAGER->value . '|' . RoleEnum::INSTALLER->value])
