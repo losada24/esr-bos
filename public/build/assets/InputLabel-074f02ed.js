ff243652e3a01ac133283af410c867beabd274e4
katiuska28 <katiuska28@gmail.com>
2026-06-29 12:18:18 -0400

    feat: add transition component for improved UI animations and state management

---
+++ public/build/assets/InputLabel-074f02ed.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-306310bf.js";function a({value:t,className:e="",children:o,...r}){return n.jsx("label",{...r,className:"block font-medium text-sm text-gray-700 "+e,children:t!==void 0})}export{a as I};
---
+++ public/build/assets/ReportInstallerCommon-b550b2cf.js
@@ -0,0 +1,1 @@
+import{c as e,b as r}from"./object-24a21c95.js";const o=e({id:r()});export{o};
