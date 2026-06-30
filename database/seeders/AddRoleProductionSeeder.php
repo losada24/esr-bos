9336a505dd95994a377ac2fc53ac49b35816546c
katiuska28 <katiuska28@gmail.com>
2026-06-30 14:02:58 -0400

    feat: add production role and update permissions across the application
    - Added 'production' role to the roles constants.
    - Implemented isProduction utility function to check for the production role.
    - Updated Sidebar component to include production role in visibility checks.
    - Modified OrderView component to incorporate production role in permission checks for viewing and editing pipelines.
    - Adjusted web routes to allow access for the production role in various endpoints related to orders and esr-process.

---
+++ database/seeders/AddRoleProductionSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\RoleEnum;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class AddRoleProductionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::firstOrCreate(['name' => RoleEnum::PRODUCTION->value]);
+    }
+}
--- public/build/assets/ReportInstallerCommon-c660e5d9.js
+++
@@ -1,1 +0,0 @@
-import{c as e,b as r}from"./array-3ca9cc57.js";const o=e({id:r()});export{o};
