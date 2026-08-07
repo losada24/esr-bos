d9f88a6b55951b3e1ae827e515b5bc811cd034e3
katiuska28 <katiuska28@gmail.com>
2026-08-07 12:23:49 -0400

    Refactor code structure for improved readability and maintainability

---
+++ public/build/assets/CloseIcon-b419ace4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-314dc2b8.js";const e=()=>o.jsxs("svg",{width:"24px",height:"24px",viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"1.5",strokeLinecap:"round",strokeLinejoin:"round",className:"h-5 w-5",children:[o.jsx("line",{x1:"18",y1:"6",x2:"6",y2:"18"}),o.jsx("line",{x1:"6",y1:"6",x2:"18",y2:"18"})]}),n=e;export{n as C};
---
+++ public/build/assets/ReportInstallerCommon-cf9d7edc.js
@@ -0,0 +1,1 @@
+import{c as e,b as r}from"./array-a9cc2bf5.js";const o=e({id:r()});export{o};
